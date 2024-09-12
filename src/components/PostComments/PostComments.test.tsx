6e53d20a827ef7cc3eb8aa2d2a56779ef121f260
FabioMedeiros1000 <fabio.leandro.medeiros@gmail.com>
2024-09-12 19:32:54 -0300

    Fazendo o teste de adição de dois comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
@@ -7,4 +6,18 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Deve adicionar dois comentários', () => {
+        render(<PostComment />);
+
+        fireEvent.change(screen.getByTestId('input-comentario'), { target: { value: 'opa' } });
+        fireEvent.click(screen.getByTestId('btn-comentar'));
+
+        expect(screen.getByText('opa')).toBeInTheDocument();
+
+        fireEvent.change(screen.getByTestId('input-comentario'), { target: { value: 'testando' } });
+        fireEvent.click(screen.getByTestId('btn-comentar'));
+
+        expect(screen.getByText('testando')).toBeInTheDocument();
+    })
 });
